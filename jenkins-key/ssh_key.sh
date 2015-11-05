d7fe29b576338f7eefe54a1b1ebd1af23f91a943
vs-zhang <shipei1209@gmail.com>
2015-11-05 18:50:54 -0500

    finish jenkins-key container Dockerfile

--- jenkins-key/ssh_key.sh
+++ jenkins-key/ssh_key.sh
@@ -6,9 +6,3 @@ if [ ! -f id_rsa.pub ]; then
         echo "Execute ssh-keygen --[done]"
 fi
 
-
-cp id_rsa jenkins-master
-echo "Copy private key to master folder --[done]"
-
-cp id_rsa.pub jenkins-slave
-echo "Copy public key to slave folder --[done]"
